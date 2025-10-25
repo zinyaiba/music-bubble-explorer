f6c846345609b76c36584efa9afa772d89d06374
zinyaiba <zinyaiba@gmail.com>
2025-10-25 20:26:26 +0900

    Updates

---
+++ assets/js/sharing-efc730e3.js
@@ -0,0 +1,1 @@
+import{D as o}from"./index-3bdd92be.js";import"./vendor-b36e4360.js";import"./styled-99d924a4.js";const t={method:o.GITHUB_ISSUES,github:{repo:{}.REACT_APP_GITHUB_REPO||"",token:{}.REACT_APP_GITHUB_TOKEN||"",labels:["song-registration","community-contribution"]}};export{t as SHARING_CONFIG};
